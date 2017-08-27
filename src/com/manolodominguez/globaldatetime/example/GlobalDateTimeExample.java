4e86e1a8fd1de9aab7b5b0cdf47e49229d4cb54c
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2017-08-27 22:13:57 +0200

    Removed a comment

--- src/com/manolodominguez/globaldatetime/example/GlobalDateTimeExample.java
+++ src/com/manolodominguez/globaldatetime/example/GlobalDateTimeExample.java
@@ -22,7 +22,6 @@ import java.time.temporal.ChronoUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * This class implements an example of use of GlobalDateTime.
  *
@@ -42,33 +41,32 @@ public class GlobalDateTimeExample {
      * @since 1.0
      */
     public static void main(String[] args) {
-        
+
         GlobalDateTime gdt1;
         GlobalDateTime gdt2;
         GlobalDateTime gdt3;
         GlobalDateTime gdt4;
         Timestamp ts;
-                
+
         try {
             // Imagine different situations:
             //
             // gdt1 will represent the current datetime of the system using the 
             // system default ZoneID.
-            gdt1= new GlobalDateTime();
+            gdt1 = new GlobalDateTime();
             // gdt2 will be a datetime that has arrived to the system via 
             // Internet in a JSON message, from Chicago. It comes in 
             // ZonedDateTime String format.
-            gdt2 = new GlobalDateTime ("2017-08-20T14:20:18.811-05:00[America/Chicago]");
+            gdt2 = new GlobalDateTime("2017-08-20T14:20:18.811-05:00[America/Chicago]");
             // gdt3 will be a datetime that has been loaded from a local log 
             // file. It is in millis from Epoch format.
-            gdt3 = new GlobalDateTime (1428348018845L);
+            gdt3 = new GlobalDateTime(1428348018845L);
             // gdt4 is a datetime that has been retrieved via JDBC from a 
             // database in Timestamp format.
             ts = new Timestamp(1428348018845L);
-            gdt4 = new GlobalDateTime (ts);
-            
+            gdt4 = new GlobalDateTime(ts);
+
             // Let's go
-            
             // Printing all datetimes. They are normalized to the same reference
             // ZoneID (Europe/Madrid by default).
             System.out.println("gdt1: " + gdt1.toNormalizedDateTimeString());
@@ -76,7 +74,7 @@ public class GlobalDateTimeExample {
             System.out.println("gdt3: " + gdt3.toNormalizedDateTimeString());
             System.out.println("gdt4: " + gdt4.toNormalizedDateTimeString());
             System.out.println();
-            
+
             // Now printing all datetimes. They are normalized to the same 
             // reference ZoneID (Europe/Madrid by default).
             gdt1.changeDefaultZone("Europe/Lisbon");
@@ -93,24 +91,25 @@ public class GlobalDateTimeExample {
             // Let's compare.
             System.out.println("Comparing gdt1 and gdt2 (both normalized)");
             if (gdt1.isBefore(gdt2)) {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened before "+ gdt2.toNormalizedDateTimeString());
-            } if (gdt1.isAfter(gdt2)) {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened after "+ gdt2.toNormalizedDateTimeString());
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened before " + gdt2.toNormalizedDateTimeString());
+            }
+            if (gdt1.isAfter(gdt2)) {
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened after " + gdt2.toNormalizedDateTimeString());
             } else {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened at the same time than "+ gdt2.toNormalizedDateTimeString());
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened at the same time than " + gdt2.toNormalizedDateTimeString());
             }
             System.out.println();
-
 
             // Let's compare again.
             System.out.println("Comparing again gdt1 and gdt2 (using different Zone ID)");
             gdt1.changeDefaultZone("Pacific/Tahiti");
             if (gdt1.isBefore(gdt2)) {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened before "+ gdt2.toNormalizedDateTimeString());
-            } if (gdt1.isAfter(gdt2)) {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened after "+ gdt2.toNormalizedDateTimeString());
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened before " + gdt2.toNormalizedDateTimeString());
+            }
+            if (gdt1.isAfter(gdt2)) {
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened after " + gdt2.toNormalizedDateTimeString());
             } else {
-                System.out.println(gdt1.toNormalizedDateTimeString()+" happened at the same time than "+ gdt2.toNormalizedDateTimeString());
+                System.out.println(gdt1.toNormalizedDateTimeString() + " happened at the same time than " + gdt2.toNormalizedDateTimeString());
             }
             System.out.println();
 
@@ -119,11 +118,12 @@ public class GlobalDateTimeExample {
             gdt3.changeDefaultZone("Australia/Sydney");
             gdt4.changeDefaultZone("Antarctica/South_Pole");
             if (gdt3.isBefore(gdt4)) {
-                System.out.println(gdt3.toNormalizedDateTimeString()+" happened before "+ gdt4.toNormalizedDateTimeString());
-            } if (gdt3.isAfter(gdt4)) {
-                System.out.println(gdt3.toNormalizedDateTimeString()+" happened after "+ gdt4.toNormalizedDateTimeString());
+                System.out.println(gdt3.toNormalizedDateTimeString() + " happened before " + gdt4.toNormalizedDateTimeString());
+            }
+            if (gdt3.isAfter(gdt4)) {
+                System.out.println(gdt3.toNormalizedDateTimeString() + " happened after " + gdt4.toNormalizedDateTimeString());
             } else {
-                System.out.println(gdt3.toNormalizedDateTimeString()+" happened at the same time than "+ gdt4.toNormalizedDateTimeString());
+                System.out.println(gdt3.toNormalizedDateTimeString() + " happened at the same time than " + gdt4.toNormalizedDateTimeString());
             }
             System.out.println();
 
@@ -133,10 +133,10 @@ public class GlobalDateTimeExample {
             gdt3.resetToDefaultZone();
             gdt4.resetToDefaultZone();
             System.out.println("Generating MySQL DATETIME format to be used in an INSERT statement");
-            System.out.println("gdt1: "+gdt1.toNormalizedMySQLDateTime());
-            System.out.println("gdt2: "+gdt2.toNormalizedMySQLDateTime());
-            System.out.println("gdt3: "+gdt3.toNormalizedMySQLDateTime());
-            System.out.println("gdt4: "+gdt4.toNormalizedMySQLDateTime());
+            System.out.println("gdt1: " + gdt1.toNormalizedMySQLDateTime());
+            System.out.println("gdt2: " + gdt2.toNormalizedMySQLDateTime());
+            System.out.println("gdt3: " + gdt3.toNormalizedMySQLDateTime());
+            System.out.println("gdt4: " + gdt4.toNormalizedMySQLDateTime());
             System.out.println();
 
             // Now we modify our GlobalDateTimes a bit.
@@ -145,35 +145,34 @@ public class GlobalDateTimeExample {
             gdt3.decrease(1, ChronoUnit.YEARS);
             gdt4.decrease(1, ChronoUnit.MONTHS);
             System.out.println("Let's change our GlobalDateTimes a bit");
-            System.out.println("gdt1 + 3 days: "+gdt1.toNormalizedMySQLDateTime());
-            System.out.println("gdt2 + 4 minutes: "+gdt2.toNormalizedMySQLDateTime());
-            System.out.println("gdt3 - 1 year: "+gdt3.toNormalizedMySQLDateTime());
-            System.out.println("gdt4 - 1 month: "+gdt4.toNormalizedMySQLDateTime());
+            System.out.println("gdt1 + 3 days: " + gdt1.toNormalizedMySQLDateTime());
+            System.out.println("gdt2 + 4 minutes: " + gdt2.toNormalizedMySQLDateTime());
+            System.out.println("gdt3 - 1 year: " + gdt3.toNormalizedMySQLDateTime());
+            System.out.println("gdt4 - 1 month: " + gdt4.toNormalizedMySQLDateTime());
             System.out.println();
 
             // Fuzzy comparation.
             if (gdt3.isOverSinceMoreThan(3, ChronoUnit.YEARS)) {
-                System.out.println("gdt3 "+gdt3.toNormalizedDateTimeString()+" happened since more than 4 years");
+                System.out.println("gdt3 " + gdt3.toNormalizedDateTimeString() + " happened since more than 4 years");
             } else {
-                System.out.println("gdt3 "+gdt3.toNormalizedDateTimeString()+" happened since less than 4 years");
+                System.out.println("gdt3 " + gdt3.toNormalizedDateTimeString() + " happened since less than 4 years");
             }
             gdt1.resetToOriginal();
             gdt1.increase(3, ChronoUnit.DAYS);
             if (gdt1.isComingInLessThan(7, ChronoUnit.DAYS)) {
-                System.out.println("gdt1 "+gdt1.toNormalizedDateTimeString()+" will happen in les than 7 days");
+                System.out.println("gdt1 " + gdt1.toNormalizedDateTimeString() + " will happen in les than 7 days");
             } else {
-                System.out.println("gdt1 "+gdt1.toNormalizedDateTimeString()+" will happen in more than 7 days");
+                System.out.println("gdt1 " + gdt1.toNormalizedDateTimeString() + " will happen in more than 7 days");
             }
 
             System.out.println();
 
         } catch (GlobalDateTimeException ex) {
             logger.error("An error creating a GlobalDateTime has happened: ", ex);
-            //Logger.getLogger(GlobalDateTimeExample.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+
     }
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalDateTimeExample.class);
-    
+
 }
